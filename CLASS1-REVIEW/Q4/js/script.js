9d150b7dd3f066f2a076dfdbe27701cb5c0c3674
Lia <aliyahrenomeron7@gmail.com>
2026-01-14 20:47:10 -0500

    added the three rectangles, their positioning and fill

--- CLASS1-REVIEW/Q4/js/script.js
+++ CLASS1-REVIEW/Q4/js/script.js
@@ -1,5 +1,34 @@
 "use strict";
 
+let rect1 = {
+    x: 0, 
+    y: 0, 
+    w: 200, 
+    h: 600,
+    r: 33, 
+    g: 55,
+    b: 255
+}
+
+let rect2 = {
+    x: 200, 
+    y: 0, 
+    w: 200, 
+    h: 600,
+    r: 46,
+    g: 64, 
+    b: 166
+}
+
+let rect3 = {
+    x: 400, 
+    y: 0, 
+    w: 200, 
+    h: 600,
+    r: 26,
+    g: 37,
+    b: 100
+}
 
 function setup() {
     createCanvas(600, 600);
@@ -9,30 +38,33 @@ function draw() {
     background(0);
     noStroke();
 
-    drawEllipse(20, 15, 15, 19, 156, 235);
-    drawEllipse(40, 50, 30, 185, 19, 235);
-    drawEllipse(70, 100, 60, 69, 19, 235);
-
-    // Draws the ellipses
-    // push();
-    // fill(19, 156, 235);
-    // ellipse(20, 15, 15);
-    // pop();
-
-    // push();
-    // fill(185, 19, 235);
-    // ellipse(40, 50, 30);
-    // pop();
-
-    // push();
-    // fill(69, 19, 235);
-    // ellipse(70,100, 60);
-    // pop();
+    drawRect1();
+    drawRect2();
+    drawRect3();
+    
 }
 
-function drawEllipse(x,y,w,r,g,b) {
+function drawRect1() {
     push();
-    fill(r, g, b);
-    ellipse(x, y, w);
+    fill(rect1.g, rect1.g, rect1.b);
+    rect(rect1.x, rect1.y, rect1.w, rect1.h);
     pop();
+}
+
+function drawRect2() {
+    push();
+    fill(rect2.r, rect2.g, rect2.b);
+    rect(rect2.x, rect2.y, rect2.w, rect2.h);
+    pop();
+}
+
+function drawRect3() {
+    push();
+    fill(rect3.r, rect3.g, rect3.b);
+    rect(rect3.x, rect3.y, rect3.w, rect3.h);
+    pop();
+}
+
+function checkOverlap(x, y, w, h, r, g, b) {
+    
 }
