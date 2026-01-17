00744607e8d2858f0d340779b12b963f3724f28c
Lia <aliyahrenomeron7@gmail.com>
2026-01-17 13:50:41 -0500

    started question 6

--- CLASS1-REVIEW/Q6/js/script.js
+++ CLASS1-REVIEW/Q6/js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+let test = {
+    textSize: 28,
+    fill: {
+        r: 255,
+        g: 255,
+        b: 255
+    }
+}
 
 function setup() {
     createCanvas(600, 600);
@@ -7,32 +15,5 @@ function setup() {
 
 function draw() {
     background(0);
-    noStroke();
-
-    drawEllipse(20, 15, 15, 19, 156, 235);
-    drawEllipse(40, 50, 30, 185, 19, 235);
-    drawEllipse(70, 100, 60, 69, 19, 235);
-
-    // Draws the ellipses
-    // push();
-    // fill(19, 156, 235);
-    // ellipse(20, 15, 15);
-    // pop();
-
-    // push();
-    // fill(185, 19, 235);
-    // ellipse(40, 50, 30);
-    // pop();
-
-    // push();
-    // fill(69, 19, 235);
-    // ellipse(70,100, 60);
-    // pop();
 }
 
-function drawEllipse(x,y,w,r,g,b) {
-    push();
-    fill(r, g, b);
-    ellipse(x, y, w);
-    pop();
-}
