b0cc80623944e76fe496f04020d9a9f40784a3f0
Lia <aliyahrenomeron7@gmail.com>
2026-01-17 16:37:57 -0500

    added the for loop

--- CLASS1-REVIEW/Q6/js/script.js
+++ CLASS1-REVIEW/Q6/js/script.js
@@ -1,28 +1,38 @@
 "use strict";
 
 let test = {
-    text: "TEST",
-    size: 28,
-    color: 255,
-    x: 300,
-    y: 300
-}
+  text: "TEST",
+  size: 28,
+  color: 255,
+  x: 300,
+  y: 300,
+};
+
+let i = 0;
 
 function setup() {
-    createCanvas(600, 600);
+  createCanvas(600, 600);
 }
 
 function draw() {
-    background(0);
+  background(0);
 
-    displayText();
+  displayText();
 }
 
 function displayText() {
+  push();
+  fill(test.color);
+  textSize(test.size);
+  textAlign(CENTER, CENTER);
+  text(test.text, test.x, test.y);
+  pop();
+
+  for (i = 0; i < 10; i++) {
     push();
     fill(test.color);
     textSize(test.size);
-    textAlign(CENTER, CENTER);
-    text(test.text, test.x, test.y);
+    text(i, i * 20, 100);
     pop();
+  }
 }
