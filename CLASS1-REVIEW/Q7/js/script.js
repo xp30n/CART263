6122665f4003d7ab034fc2dc918b2ecab7ba02ef
Lia <aliyahrenomeron7@gmail.com>
2026-01-17 23:16:10 -0500

    updated the color so that it's random on each reload

--- CLASS1-REVIEW/Q7/js/script.js
+++ CLASS1-REVIEW/Q7/js/script.js
@@ -1,18 +1,27 @@
 
 "use strict";
 
+let r, g, b;
+
 function setup() {
-    createCanvas(600, 600);
+  createCanvas(600, 600);
+
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-    background(0);
+  background(0);
 
-    for (let x = 0; x <= width; x +=50) {
-        for (let y = 0; y <= height; y +=50) {
-            fill(0, 0, 255);
-            ellipse(x, y, 45, 45);
-        }
+  drawElllipses();
+}
+
+function drawElllipses() {
+    fill(r, g, b);
+  for (let x = 0; x <= width; x += 50) {
+    for (let y = 0; y <= height; y += 50) {
+      ellipse(x, y, 45, 45);
     }
-    
+  }
 }
