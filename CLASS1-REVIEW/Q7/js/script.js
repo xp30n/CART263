55b1671fcd0b86063412b6d7c3bb8a1c85aec476
Lia <aliyahrenomeron7@gmail.com>
2026-01-17 23:06:22 -0500

    added a nested loop to create ellipses

--- CLASS1-REVIEW/Q7/js/script.js
+++ CLASS1-REVIEW/Q7/js/script.js
@@ -1,5 +1,5 @@
-"use strict";
 
+"use strict";
 
 function setup() {
     createCanvas(600, 600);
@@ -7,5 +7,12 @@ function setup() {
 
 function draw() {
     background(0);
+
+    for (let x = 0; x <= width; x +=50) {
+        for (let y = 0; y <= height; y +=50) {
+            fill(0, 0, 255);
+            ellipse(x, y, 45, 45);
+        }
+    }
     
 }
