1af98ef0f514cab31982d1f10cc6df55d07c4ffb
Lia <aliyahrenomeron7@gmail.com>
2026-01-28 19:45:19 -0500

    finished with part 2 of the lesson

--- WEEK3/js/3_events.js
+++ WEEK3/js/3_events.js
@@ -4,22 +4,41 @@ function setup() {
     console.log("events!")
 
     let introSection = document.querySelector("#intro");
-
-    let s1 = document.querySelector("#s1");
-
     // to allow this specific section to become clickable - no need for collision detection !
     introSection.addEventListener("click", mouseClickCallback); 
-    
     // the mouseClickCallback is a function that we are creating - Make sure not to add the parentheses after calling the function, because then it would run automatically and not only during the event! 
 
+    let s1 = document.querySelector("#s1");
     s1.addEventListener("click", mouseClickS1);
 
-    function mouseClickCallback() {
-        console.log("Prepare for my arrival worm");
+    let s2 = document.querySelector("#s2");
+    s2.addEventListener("click", mouseClickS1);
+
+
+    function mouseClickCallback(eventObj) {
+        console.log("PREPARE FOR MY ARRIVAL WORM.");
+        console.log(this); // this specific line of code tells you exactly which element was clicked !! 
+        console.log(eventObj);
+        
+        let idOfThis = this.getAttribute.id; // This is basically saying to get the ID of the variable then logging it in the console
+        // console.log(document.querySelector(`#${idOfThis} p` )) // you need to use `` in order for this line of code to work
+        // `#${idOfThis} p` // this line is basically getting the value out of this p element
+
+        // this.style.background = "royalblue"; // you can also use "this" to easily access the specifc section
+
+        // you can also do console.log(eventObj) and place "eventObj" in the parameters section, which basically tells you all the information of the specific event. Can be useful
+
+        // you can also use "target" in place of "this" which will only target one specifc element based on the mouse position, and not the entire section
+
+        let child = document.querySelector(`#${idOfThis} p`) // this is getting the child of the parent
+        let classToAdd = `#${idOfThis}-section-active`; 
+        this.classList.add(classToAdd)
+        let classToAddP =  `#${idOfThis} -section-p-active`;
+        child.classList.add(classToAddP);
     }
     
     function mouseClickS1() {
-        console.log("CECIL !! ")
+        console.log("CECIL! I NEED YOU ! ")
+        console.log(this);
     }
-
 }
