c5d52cb4831c8f11093fb196108d43fcf6531a35
Lia <aliyahrenomeron7@gmail.com>
2026-02-04 22:45:06 -0500

    base syntax for mouseMove function

--- TASK3/task-3-js/team-a.js
+++ TASK3/task-3-js/team-a.js
@@ -24,24 +24,19 @@ function setup_A() {
 
   function aniA(parentCanvas) {
     console.log("in ani-A -teamA");
+
+    // * First you need to figure out how to console log the mouse position within the box
+
+    document.querySelector(".ani_canvA_A").addEventListener("mousemove", mouseMoved);
+
+
+    function mouseMove() {
+      // this is where I will store the function to track where the mouse is on the canvas
+    }
   }
 
 
-  /****************ANI B ************************************ */
-  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN B INSIDE HERE */
-  /****************ANI B ************************************ */
-  /**************** TASK *******************************************
-   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
-   * 1: create a creatve, visual pattern using text, divs as shapes, images ... 
-   * 2: add in mouseover event listener(s) somewhere to make the sketch interactive
-   *
-   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
-   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
-   * this is so that your styles are not overriden by other teams.
-   * NOTE::: All your code is to be added here inside this function -
-   * remember you can define other functions inside....
-   * Do not change any code above or the HTML markup.
-   * **/
+
 
   function aniB(parentCanvas) {
       console.log("in ani-B -teamA");
