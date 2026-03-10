6059777ffa4bf65d9562e38916d278811033631f
Lia <aliyahrenomeron7@gmail.com>
2026-03-10 19:32:21 -0400

    changed the char functions and added mouse click events

--- PROJ-1/js/Box.js
+++ PROJ-1/js/Box.js
@@ -2,8 +2,12 @@ window.onload = function () {
   let lightOn = false;
   let charState = "sit";
   let charIndex = 0;
-  const FADE_TIME = 400;
+  const fadeTime = 400;
   let charAnimating = false;
+  let charHearts = 0;
+  let heartVisible = false;
+  let charNavBox = this.document.querySelector("#charNavBox");
+  let heartChance = 1;
   let layout = {
     background: {
       div: document.createElement("div"),
@@ -18,11 +22,9 @@ window.onload = function () {
 
     lights: {
       el: document.createElement("img"),
-      // For the sources I'm thinking of adding them to an array so its  stored in just one variable
       offSrc: "/images/light-on.png",
       onSrc: "/images/light-on.png",
       srcPaths: ["images/light-off.png", "images/light-on.png"],
-      image: "",
     },
     dispenser: {
       el: document.createElement("img"),
@@ -41,35 +43,14 @@ window.onload = function () {
         "images/character-push.png",
         "images/character-pick.png",
       ],
-      image: "",
+      states: ["sit", "stand", "push", "pick"],
     },
   };
-  let notifBtnId = this.document.querySelector("#notify");
-  notifBtnId.addEventListener("click", mouseClickNotif);
-  console.log(notifBtnId, lightOn);
   /**
-   * Checks when the notif button is clicked
+   * Add charNavBox to the DOM
    */
-  function mouseClickNotif() {
-    lightOn = !lightOn;
-    updateLight();
-    layout.lights.el.src = layout.lights.image;
-    // Checks the character animation
-    if (charState === "sit" && lightOn && !charAnimating) {
-      charState = "stand";
-      charAnimating = true;
-      cycleCharacter();
-    }
-  }
-  /**
-   * Changes light image to On or Off
-   */
-  function updateLight() {
-    if (lightOn) {
-      layout.lights.image = layout.lights.srcPaths[1];
-    } else {
-      layout.lights.image = layout.lights.srcPaths[0];
-    }
+  function renderCharNavBox() {
+    charNavBox.textContent = `❤️: ${charHearts}`;
   }
   /**
    * Add images to the DOM
@@ -80,7 +61,7 @@ window.onload = function () {
     layout.background.image.src = "images/background.png";
     layout.background.div.appendChild(layout.background.image);
     document.getElementsByTagName("main")[0].appendChild(layout.background.div);
-    //box // right here
+    //box
     layout.box.el.src = "images/box.png";
     layout.box.el.id = "box";
 
@@ -95,11 +76,12 @@ window.onload = function () {
     document.querySelector(".layout").appendChild(layout.grid.el);
 
     // light
-    layout.lights.el.src = layout.lights.image;
+    layout.lights.el.src = layout.lights.srcPaths[0];
     layout.lights.el.id = "light";
     document.querySelector(".layout").appendChild(layout.lights.el);
     /**
      * LEON GOONER #2 AREA 😋 // I think we should leave these comments in they're so funny LMFAO PLSKDSJDJ
+     * hey silly check the chat
      */
     //dispenser
     layout.dispenser.el.src = "images/dispenser.png";
@@ -118,33 +100,121 @@ window.onload = function () {
     layout.character.el.id = "char";
     document.querySelector(".layout").appendChild(layout.character.el);
   }
+
+  /**
+   *  ALL MOUSE CLICK EVENTS 🖱️🖱️🖱️🖱️
+   */
+
+  /**
+   * Sets the notif button id variable and calls the mouse click event for the notif button
+   */
+  let notifBtnId = document.querySelector("#notify");
+  notifBtnId.addEventListener("click", mouseClickNotif);
+
+  /**
+   * Checks when the notif button is clicked
+   */
+  function mouseClickNotif() {
+    if (!charAnimating && !lightOn) {
+      lightOn = true;
+    }
+    updateLight();
+
+    // Checks the character animation and calls the cycle charcater animation function
+    if (charState === "sit" && lightOn && !charAnimating) {
+      charState = "stand";
+      charAnimating = true;
+      cycleCharacter();
+    }
+  }
+  let heartVariabilityInput = document.querySelector("#heartVariability");
+  heartVariabilityInput.addEventListener("click", mouseClickheartVariable);
+
+  function mouseClickheartVariable() {
+    heartChance = heartVariabilityInput.value;
+  }
+  /**
+   *  SPECIFIC EVENTS
+   */
+  /**
+   * Changes light image to On or Off
+   */
+  function updateLight() {
+    if (lightOn) {
+      layout.lights.el.src = layout.lights.srcPaths[1];
+    } else {
+      layout.lights.el.src = layout.lights.srcPaths[0];
+    }
+  }
+
   /**
    * Cycle through character animation
    */
   function cycleCharacter() {
-    // stop at the last state (pick)
-    if (charIndex === layout.character.srcPaths.length - 1) {
-      charState = "pick";
-      charAnimating = false;
-      return;
-    }
-    console.log(charIndex);
     // fade out
     layout.character.el.style.opacity = 0;
 
     setTimeout(() => {
       // go to next state
       charIndex++;
+      // if we reached the end (pick), go back to sit and stop and turn off the light
+      if (charIndex >= layout.character.srcPaths.length) {
+        charIndex = 0;
+        layout.character.el.src = layout.character.srcPaths[0];
+        charState = "sit";
+        char.style.opacity = 1;
+        // Remove the heart when the character goes to sit
+        dropHeartCheck(charState);
+        // Turn off the light when the character goes back to sit
+        lightOn = false;
+        updateLight();
+        charAnimating = false;
+        return;
+      }
       // change image after fade out
       layout.character.el.src = layout.character.srcPaths[charIndex];
-      const states = ["sit", "stand", "push", "pick"];
-      charState = states[charIndex];
+      charState = layout.character.states[charIndex];
+      radomizeHeartDrop(charState);
+      dropHeartCheck(charState);
       // fade in
       char.style.opacity = 1;
       // continue animation
-      setTimeout(cycleCharacter, FADE_TIME);
-    }, FADE_TIME); // same time as CSS transition
+      setTimeout(cycleCharacter, fadeTime);
+    }, fadeTime); // same time as CSS transition
   }
+  /**
+   * Check if the character pushed the button to drop a heart (make the heart's display visible or invisible)
+   */
+  function dropHeartCheck(state) {
+    if (state == "pick" && heartVisible) {
+      charHearts++;
+      renderCharNavBox();
+    }
+    if (state == "push" || state == "pick") {
+      if (heartVisible) {
+        layout.heart.el.style.display = "block";
+      }
+    } else {
+      heartVisible = false;
+      layout.heart.el.style.display = "none";
+    }
+  }
+  /**
+   * Randomize Heart Visibility when character pushes button(based on variability slider input)
+   */
+  function radomizeHeartDrop(state) {
+    if (state == "push") {
+      const randomNumber = Math.floor(Math.random() * heartChance) + 1;
+      console.log(randomNumber, heartChance);
+      if (randomNumber === 1) {
+        heartVisible = true;
+      } else {
+        heartVisible = false;
+      }
+    }
+  }
+  dropHeartCheck(charState);
   updateLight();
-renderlayout();
+  renderlayout();
+  renderCharNavBox();
 };
