1bc20b3d31dcc5ba8922de4a4d438efdb977eaac
Lia <aliyahrenomeron7@gmail.com>
2026-03-21 14:23:28 -0400

    added base files for project

---
+++ PROJ-2/js/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 370,
+        height: 470,
+        resizable: false,
+        mazimizable: false,
+        fullscreenable: true,
+        frame: false,
+        transparent: true,
+        backgroundColor: '#00000000',
+        webPreferences: {
+            contextIsolation: true
+        }
+    })
+
+    win.loadFile('index.html');
+    // win.setAlwaysOnTop(true);
+}
+
+app.whenReady().then(createWindow)
