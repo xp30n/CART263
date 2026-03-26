40cfb865729430113e9365500af5e389d197cfe3
Lia <aliyahrenomeron7@gmail.com>
2026-03-26 13:29:09 -0400

    changed to start only on load

--- PROJ-2/js/script.js
+++ PROJ-2/js/script.js
@@ -1,6 +1,10 @@
-let startBtn = document.getElementById("start-btn")
 
-startBtn.addEventListener("click", function () {
-   // console.log("Button is clicked")
-   window.location.href="#"
-})
+// Wait until everything is loaded
+window.addEventListener("load", function () {
+  let startBtn = document.getElementById("start-btn");
+
+  startBtn.addEventListener("click", function () {
+    // console.log("Button is clicked")
+    window.location.href = "#";
+  });
+});
